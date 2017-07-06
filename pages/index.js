8748522eb71053425fe775e5d56c940bb57c2784
teddy-lun <teddy-lun@en-trak.com>
2017-07-07 00:45:10 +0800

    Lesson2 - Navigation between Page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+// this is the Link API
+import Link from 'next/link'
+
 const Index = () => (
-    <div>
-        <p>Hello Next.js</p>
-    </div>
+  <div>
+    <Link href="/about">
+      <a style={{ fontSize: 20 }}>About Page</a>
+      {/*<button>Go to About Page</button>*/}
+      {/*<div>Go to About Page</div>*/}
+    </Link>
+    <p>Hello Next.js</p>
+  </div>
 )
 
 export default Index
