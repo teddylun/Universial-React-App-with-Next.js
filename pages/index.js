4b4694590cf2f4ea98d02541d7dea724acae93c6
teddy-lun <teddy-lun@en-trak.com>
2017-07-09 22:53:47 +0800

    lesson8 styling index page done

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,69 @@
-// pages/index.js
-
 import Layout from '../components/MyLayout.js'
 import Link from 'next/link'
 
-const PostLink = (props) => (
+function getPosts () {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js'},
+    { id: 'learn-nextjs', title: 'Learn Next.js is awesome'},
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT'},
+  ]
+}
+
+const PostLink = ({ post }) => (
   <li>
-    <Link as={`/post/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
+    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+      <a>{post.title}</a>
     </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 )
+
+
 export default () => (
-    <Layout>
-      <h1>Next.js Course</h1>
-      <ul>
-        <PostLink id="basic-setup-of-nextjs" title="Basic setup of Next.js"/>
-        <PostLink id="navagtion-between-pages" title="Navigation between Pages"/>
-        <PostLink id="using-shared-component" title="Using shared component"/>
-        <PostLink id="create-dynamic-pages" title="Create Dynamic Pages"/>
-      </ul>
-    </Layout>
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      {getPosts().map((post) => (
+        <PostLink key={post.id} post={post}/>
+      ))}
+    </ul>
+    <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </Layout>
 )
