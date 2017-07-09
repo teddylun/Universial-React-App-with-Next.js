9434feccb785cfbf17b18a2d7a85d6e316e08709
teddy-lun <teddy-lun@en-trak.com>
2017-07-09 13:27:53 +0800

    lesson 5 done

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 const PostLink = (props) => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
+    <Link as={`/course/${props.id}`} href={`/course?title=${props.title}`}>
       <a>{props.title}</a>
     </Link>
   </li>
@@ -14,10 +14,10 @@ export default () => (
     <Layout>
       <h1>Next.js Course</h1>
       <ul>
-        <PostLink title="Basic setup of Next.js"/>
-        <PostLink title="Navigation between Pages"/>
-        <PostLink title="Using shared component"/>
-        <PostLink title="Create Dynamic Pages"/>
+        <PostLink id="basic-setup-of-nextjs" title="Basic setup of Next.js"/>
+        <PostLink id="navagtion-between-pages" title="Navigation between Pages"/>
+        <PostLink id="using-shard-component" title="Using shared component"/>
+        <PostLink id="create-dynamic-pages" title="Create Dynamic Pages"/>
       </ul>
     </Layout>
 )
